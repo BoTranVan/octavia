7565071d960b4ca5d62a98a540af680168e19533
yangjianfeng <yangjf@chinatelecom.cn>
2019-01-22 12:39:23 -0800

    Fix a topology bug in flavors support
    This patch fixes an oversight in the addition of flavors support in the
    amphora driver[1]. The amphora-agent configuration file was still getting
    the topology selected in the configuration file as opposed to the
    topology selected in the flavor.
    
    This is an additional patch at the end of the chain as it leverages
    changes that were made in later flavor patches that pass the flavor
    into the taskflow flow.
    
    A follow on patch will address spares pool amphora.
    
    [1] https://review.openstack.org/#/c/621323
    
    jira: http://172.18.211.163:8080/browse/NEUT-147
    origin: https://review.openstack.org/#/c/632551/2
    Change-Id: Iba62df76a8dc03b2c4a5bed18f9cf94f50f19e9d

--- octavia/amphorae/backends/agent/agent_jinja_cfg.py
+++ octavia/amphorae/backends/agent/agent_jinja_cfg.py
@@ -34,7 +34,7 @@ class AgentJinjaTemplater(object):
         self.agent_template = jinja_env.get_template(
             constants.AGENT_CONF_TEMPLATE)
 
-    def build_agent_config(self, amphora_id):
+    def build_agent_config(self, amphora_id, topology):
         return self.agent_template.render(
             {'enable_tls': CONF.amphora_agent.enable_tls,
              'agent_server_ca':
@@ -62,4 +62,4 @@ class AgentJinjaTemplater(object):
              'respawn_count': CONF.haproxy_amphora.respawn_count,
              'respawn_interval': CONF.haproxy_amphora.respawn_interval,
              'amphora_udp_driver': CONF.amphora_agent.amphora_udp_driver,
-             'topology': CONF.controller_worker.loadbalancer_topology})
+             'topology': topology})
